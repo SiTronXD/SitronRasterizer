f6a6d9ca002afe140190a9baba69e45e9bff7b9d
Bensson <47039664+SiTronXD@users.noreply.github.com>
2020-06-22 14:00:41 +0200

    Restructured code so that Eclipse's hot code replace can work

--- src/game/Main.java
+++ src/game/Main.java
@@ -1,45 +1,67 @@
 package game;
 
+import java.awt.Canvas;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
+
+import javax.swing.JFrame;
+
 import engine.Matrix;
 import engine.Renderer;
+import engine.Texture;
 import engine.Vector;
 import engine.Vertex;
 import engine.Window;
 
 public class Main {
-	public static void main(String[] args) {
-		int width = 1280;
-		int height = 720;
+	static int screenWidth = 1280;
+	static int screenHeight = 720;
+	
+	float timer;
+	
+	boolean running;
+	
+	Window window;
+	Renderer renderer;
+	
+	Matrix perspectiveTransform;
+	Matrix transform;
+	
+	Vertex v1;
+	Vertex v2;
+	Vertex v3;
+	
+	public void Init() 
+	{
+		window = new Window(screenWidth, screenHeight, "Sitron Rasterizer!!!");
+		renderer = new Renderer(screenWidth, screenHeight);
 		
-		Window window = new Window(width, height, "Sitron Rasterizer!!!");
-		Renderer renderer = new Renderer(width, height);
-		
-		Matrix screenSpaceTransform = Matrix.ScreenSpace(width, height);
-		Matrix perspectiveTransform = Matrix.Perspective(
-			(float)width / (float)height, 
+		perspectiveTransform = Matrix.Perspective(
+			(float)screenWidth / (float)screenHeight, 
 			(float) Math.toRadians(90.0f), 
 			0.1f, 
 			100.0f
 		);
 		
-		float timer = 0.0f;
-		
-		Vertex v1 = new Vertex(new Vector( 0.0f,  0.5f, 0.0f), new Vector(255, 0, 0));
-		Vertex v2 = new Vertex(new Vector(-0.5f, -0.5f, 0.0f), new Vector(0, 255, 0));
-		Vertex v3 = new Vertex(new Vector( 0.5f, -0.5f, 0.0f), new Vector(0, 0, 255));
+		v1 = new Vertex(new Vector( 0.0f,  0.5f, 0.0f), new Vector(255, 0, 0));
+		v2 = new Vertex(new Vector(-0.5f, -0.5f, 0.0f), new Vector(0, 255, 0));
+		v3 = new Vertex(new Vector( 0.5f, -0.5f, 0.0f), new Vector(0, 0, 255));
 		
 		
 		// Main loop
 		double lastTime = System.nanoTime();
-		boolean running = true;
+		running = true;
 		while(running) // JFrame is running on a different thread
 		{
 			double timeNow = System.nanoTime();
-			double deltaTime = timeNow - lastTime;
+			double deltaTime = (timeNow - lastTime) / 1000000000.0;
 			lastTime = System.nanoTime();
 			
-			double timeInMilliseconds = deltaTime / 1000000.0;
-			double fps = 1000000000.0 / deltaTime; // 1 / (deltaTime / 1000000000.0)
+			double timeInMilliseconds = deltaTime * 1000.0;
+			double fps = 1.0 / deltaTime; // 	1 / (deltaTime / 1000000000.0)
 			
 			timeInMilliseconds = (double) Math.round(timeInMilliseconds * 100.0) / 100.0;
 			fps = (double) Math.round(fps);
@@ -47,29 +69,41 @@ public class Main {
 			System.out.println("ms: " + timeInMilliseconds + "  (fps: " + fps + ")");
 			
 			
-			renderer.ClearRenderTexture(50, 50, 50);
-			
-			timer += 0.003f;
-			
-			Matrix t = Matrix.Identity();
-			t = Matrix.MatMatMul(Matrix.Translate(0.0f, 0.0f, 1.0f), t);
-			t = Matrix.MatMatMul(Matrix.RotateY(timer), t);
-			t = Matrix.MatMatMul(Matrix.Translate(0.0f, 0.0f, 2.0f), t);
-			t = Matrix.MatMatMul(perspectiveTransform, t);
-			
-			renderer.DrawTriangle(t, v1, v2, v3);
-			
-			// Rot Y from above
-			/*
+			// Eclipse's hot code replace doesn't work if the code is in this while loop :/
+			Update();
+			Render();
+		}
+	}
+	
+	void Update()
+	{
+		timer += 0.003f;
+		
+		transform = Matrix.Identity();
+		transform = Matrix.MatMatMul(Matrix.Translate(0.0f, 0.0f, 1.0f), transform);
+		transform = Matrix.MatMatMul(Matrix.RotateY(timer), transform);
+		transform = Matrix.MatMatMul(Matrix.Translate(0.0f, 0.0f, 2.0f), transform);
+		transform = Matrix.MatMatMul(perspectiveTransform, transform);
+		
+		// Rot Y from above
+		/*
 			float x = 20.0f;
 			float y = 20.0f;
 			renderer.GetRenderTexture().SetPixel(
 				100 + (int) (Math.cos(timer)*x + Math.sin(timer)*y), 
 				100 + (int) (Math.cos(timer)*y - Math.sin(timer)*x), 
 				255, 0, 0, 255);
-				*/
-			
-			window.ShowBuffer(renderer.GetRenderTexture());
-		}
+		*/
 	}
+	
+	void Render()
+	{
+		renderer.ClearRenderTexture(50, 50, 50);
+		
+		renderer.DrawTriangle(transform, v1, v2, v3);
+
+		window.ShowBuffer(renderer.GetRenderTexture());
+	}
+	
+	public static void main(String[] args) {  Main m = new Main(); m.Init(); }
 }
